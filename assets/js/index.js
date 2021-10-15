7b60c3d7828a185c32d3813fcd2a4e29061fca09
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-10-15 09:01:01 +0100

    added ibdex.js

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  //change the integers below to match the height of your upper div, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () { 
+
+    console.log($(window).scrollTop());
+
+    if ($(window).scrollTop() > 550) {
+        $('#navbar').removeClass('pt-4');
+        $('#navbar').removeClass('pb-4');
+      $('#navbar').addClass('pt-2');
+      $('#navbar').addClass('pb-2');
+    }
+
+    if ($(window).scrollTop() < 551) {
+      $('#navbar').addClass('pt-4');
+        $('#navbar').addClass('pb-4');
+      $('#navbar').removeClass('pt-2');
+      $('#navbar').removeClass('pb-2');
+    }
+  });
+});
