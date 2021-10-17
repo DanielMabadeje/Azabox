8c4cbbfd6ac4441df18744e586cb97477085f082
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-10-18 00:49:39 +0100

    Added code

--- assets/js/waitlist.js
+++ assets/js/waitlist.js
@@ -10,7 +10,7 @@ $('#my-form').submit(function(e) {
     e.preventDefault();
     //AJAX request
   
-    console.log("clicked");
+    // console.log("clicked");
 
     swal("What do you want to wait for?", {
         buttons: {
@@ -25,7 +25,32 @@ $('#my-form').submit(function(e) {
       })
       .then((value) => {
 
-        alert(value);
+        // alert(value);
+
+        var data=$('#my-form').serialize()
+        // data['entry.775823104_sentinel']=value
+
+        data["entry.775823104"]=value
+        $.ajax({
+            url: 'https://docs.google.com/forms/d/e/1WsAIEVBiVIlC7g24trGNWiNxBYuNwQZQ2o35_UwZEXg/formResponse',     //The public Google Form url, but replace /view with /formResponse
+            data: data, //Nifty jquery function that gets all the input data
+            type: 'POST', //tells ajax to post the data to the url
+            dataType: "json", //the standard data type for most ajax requests
+            statusCode: { //the status code from the POST request
+              0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+                //success
+                $('#form-success').text('hooray!');
+              },
+              200: function(data) {//200 is a success code. it went through!
+                //success
+                $('#form-success').text('hooray!');
+              },
+              403: function(data) {//403 is when something went wrong and the submission didn't go through
+                //error
+                alert('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
+              }
+            }  
+          });
         // switch (value) {
        
         //   case "defeat":
@@ -40,11 +65,12 @@ $('#my-form').submit(function(e) {
         //     swal("Got away safely!");
         // }
       });
+    //   entry.1911968125 email
+    // entry.775823104 wait for
   
-  
-    return
+    // return
     $.ajax({
-      url: 'https://docs.google.com/forms/d/e/1WsAIEVBiVIlC7g24trGNWiNxBYuNwQZQ2o35_UwZEXg/formResponse',     //The public Google Form url, but replace /view with /formResponse
+      url: 'https://docs.google.com/forms/d/e/1FAIpQLSd1WbBQC2UxvoICbi35KQBztmC5dLYDSP5O43fY3Wzb76x6xg/formResponse',     //The public Google Form url, but replace /view with /formResponse
       data: $('#my-form').serialize(), //Nifty jquery function that gets all the input data
       type: 'POST', //tells ajax to post the data to the url
       dataType: "json", //the standard data type for most ajax requests
@@ -55,6 +81,7 @@ $('#my-form').submit(function(e) {
         },
         200: function(data) {//200 is a success code. it went through!
           //success
+          swal("Gotcha!", "Pikachu was caught!", "success");
           $('#form-success').text('hooray!');
         },
         403: function(data) {//403 is when something went wrong and the submission didn't go through
