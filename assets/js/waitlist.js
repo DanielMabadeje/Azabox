dd45189953570205b305bff6e2a4972b268cbf9d
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-11-19 16:27:14 +0100

    deleted stuffs

--- assets/js/waitlist.js
+++ assets/js/waitlist.js
@@ -362,16 +362,18 @@ $('.my-form').each(function() {
           0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
             //success
             console.log("Success")
+            swal("Weldone!", "You have joined Successfully!", "success");
             // $('#form-success').text('hooray!');
           },
           200: function(data) {//200 is a success code. it went through!
             //success
             console.log("Success")
+            swal("Gotcha!", "Pikachu was caught!", "success");
             // $('#form-success').text('hooray!');
           },
           403: function(data) {//403 is when something went wrong and the submission didn't go through
             //error
-            alert('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
+            alert('Oh no! something went wrong.');
           }
         }  
     });
